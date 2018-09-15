9c3b7d64c5da4e02753740921d41ed4da59d2958
Vahagn Gevorgyan <vahagngevorgyan@icloud.com>
2018-09-15 22:44:30 +0400

    Initial commit

---
+++ SharedFreamwork/SharedFreamwork/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  SharedFreamwork
+//
+//  Created by Vahagn Gevorgyan on 15/09/2018.
+//  Copyright © 2018 Vahagn Gevorgyan. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Do some staff"
+    }
+    
+}
---
+++ SharedFreamwork/SharedFreamwork/SharedFreamwork.h
@@ -0,0 +1,19 @@
+//
+//  SharedFreamwork.h
+//  SharedFreamwork
+//
+//  Created by Vahagn Gevorgyan on 15/09/2018.
+//  Copyright © 2018 Vahagn Gevorgyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedFreamwork.
+FOUNDATION_EXPORT double SharedFreamworkVersionNumber;
+
+//! Project version string for SharedFreamwork.
+FOUNDATION_EXPORT const unsigned char SharedFreamworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedFreamwork/PublicHeader.h>
+
+
